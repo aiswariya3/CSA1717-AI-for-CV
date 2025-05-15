b76bf689442fbd62bfc6da8e01f4bb18baafe582
aiswariya3 <129868336+aiswariya3@users.noreply.github.com>
2025-05-15 10:02:22 +0530

    Add files via upload

---
+++ 16_feedforward_nn.py
@@ -0,0 +1,13 @@
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+import numpy as np
+
+X = np.array([[0,0],[0,1],[1,0],[1,1]])
+y = np.array([[0],[1],[1],[0]])
+
+model = Sequential()
+model.add(Dense(10, input_dim=2, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.fit(X, y, epochs=100, verbose=1)
